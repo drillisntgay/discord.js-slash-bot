6e240644f4ad9ddb850441ecd6497ebcb725b8cf
drillisntgay <124436220+drillisntgay@users.noreply.github.com>
2023-05-20 12:33:02 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+//made by drill
+const fs = require('node:fs');
+const path = require('node:path');
+
+const { Client, Collection, Intents } = require('discord.js');
+
+const token = process.env['TOKEN'];
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const eventsPath = path.join(__dirname, 'events'); //path to events folder based on operating system
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+
+	client.commands.set(command.data.name, command);
+}
+
+client.on('interactionCreate', async interaction => {
+
+  if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction); 
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true }); // ephemeral 
+	}
+});
+
+client.login(token);
+//made by drill
